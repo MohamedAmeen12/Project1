a2d801ccfcfd32f145593acc6bc90da717dffb4e
Khalidmohamedelshafee <168382785+Khalidmohamedelshafee@users.noreply.github.com>
2024-06-23 22:26:46 +0300

    Add files via upload

---
+++ models/Purchase.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const cartItemSchema = new mongoose.Schema(
+  {
+    carId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Car",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1,
+      default: 1,
+    },
+  },
+  { _id: false }
+);
+
+const purchaseSchema = mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  cart: [cartItemSchema],
+  totalPrice: {
+    type: Number,
+    min: 0,
+  },
+  amountPaid: {
+    type: Number,
+    min: 0,
+  },
+  time: {
+    type: Date,
+    default: Date.now,
+  },
+  purchaseType: {
+    type: String,
+    enum: ["cash", "loan"],
+    required: true,
+  },
+});
+
+const Purchase = mongoose.model("Purchase", purchaseSchema);
+
+export default Purchase;
