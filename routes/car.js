905c226d48995a521ca956fcf1a813e75d70ec8b
abdullah-165 <168745459+abdullah-165@users.noreply.github.com>
2024-06-25 05:55:58 +0300

    Update car.js

--- routes/car.js
+++ routes/car.js
@@ -1,6 +1,9 @@
 import express from "express";
+import multer from "multer";
+import path from "path";
 import {
   createCar,
+  uploadImage,
   getCars,
   getCarById,
   updateCar,
@@ -8,6 +11,44 @@ import {
 } from "../controllers/car.js";
 
 const router = express.Router();
+
+// Set storage engine
+const storage = multer.diskStorage({
+  destination: "./public/img",
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+// Initialize upload
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 }, // Limit file size to 1MB
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+}).single("image");
+
+// Check file type
+function checkFileType(file, cb) {
+  // Allowed file types
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+}
+
+router.post("/upload", upload, uploadImage);
 
 router.post("/", createCar);
 router.get("/", getCars);
