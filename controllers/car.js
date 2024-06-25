7cd52e4a945deaa3f658bd4d31c1b5fd5a9777af
abdullah-165 <168745459+abdullah-165@users.noreply.github.com>
2024-06-25 05:54:50 +0300

    Update car.js

--- controllers/car.js
+++ controllers/car.js
@@ -1,20 +1,52 @@
 import Car from "../models/Car.js";
 import mongoose from "mongoose";
 
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const createCar = async (req, res) => {
+  const carData = req.body;
   try {
-    const car = new Car(req.body);
-    await car.save();
-    res.status(201).json(car);
+    const newCar = new Car(carData);
+    await newCar.save();
+    res.json({ message: "Car added successfully" });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(500).json({ errMsg: error.message });
+  }
+};
+
+const uploadImage = async (req, res) => {
+  const imageUrl = "/img/" + req.file.filename;
+  res.json({ imageUrl });
+};
+
+const deleteCar = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const car = await Car.findById(id);
+
+    if (!car) {
+      return res.status(404).json({ errMsg: "Car not found" });
+    }
+
+    await Car.findByIdAndDelete(id);
+
+    res.status(200).json({ message: "Car deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ errMsg: error });
   }
 };
 
 const getCars = async (req, res) => {
   try {
     const cars = await Car.find();
-    res.status(200).json(cars);
+    res.json(cars);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
@@ -28,7 +60,7 @@ const getCarById = async (req, res) => {
     if (!car) {
       return res.status(404).json({ message: "Car not found" });
     }
-    res.status(200).json(car);
+    res.json(car);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -36,62 +68,21 @@ const getCarById = async (req, res) => {
 
 const updateCar = async (req, res) => {
   const { id } = req.params;
-  const {
-    type,
-    model,
-    yearOfManifacture,
-    price,
-    hoursePower,
-    cylinders,
-    transmission,
-    cc,
-    color,
-    airbags,
-    EBD,
-    ABS,
-    length,
-    width,
-    height,
-    wheelbase,
-  } = req.body;
+  const updates = req.body;
 
   if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send(`No car with id: ${id}`);
 
-  const updatedCar = {
-    type,
-    model,
-    yearOfManifacture,
-    price,
-    hoursePower,
-    cylinders,
-    transmission,
-    cc,
-    color,
-    airbags,
-    EBD,
-    ABS,
-    length,
-    width,
-    height,
-    wheelbase,
-    _id: id,
-  };
+  const updatedCar = await Car.findByIdAndUpdate(id, updates, {
+    new: true,
+    runValidators: true,
+  });
 
-  await Car.findByIdAndUpdate(id, updatedCar, { new: true });
+  if (!updatedCar) {
+    return res.status(404).json({ errMsg: "Car not found" });
+  }
 
-  res.json(updatedCar);
+  return res.status(200).json({ updatedCar });
 };
 
-const deleteCar = async (req, res) => {
-  const { id } = req.params;
-
-  if (!mongoose.Types.ObjectId.isValid(id))
-    return res.status(404).send(`No car with id: ${id}`);
-
-  await Car.findByIdAndDelete(id);
-
-  res.json({ message: "Car deleted successfully" });
-};
-
-export { createCar, getCars, getCarById, updateCar, deleteCar };
+export { createCar, uploadImage, getCars, getCarById, updateCar, deleteCar };
